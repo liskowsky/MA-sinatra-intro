47b8f41ba7405551d81ee2da6ce99b1f4b058c75
liskowsky <karollebiedzinski@gmail.com>
2016-07-19 02:20:11 +0100

    8 basic HTML returned

--- app.rb
+++ app.rb
@@ -8,6 +8,6 @@ require 'sinatra'
     "Luke, I am your papi"
   end
 
-  get '/next' do
-    "Nooo!"
+  get '/cat' do
+    "<div style='border: 3px dashed red'> <img src='http://bit.ly/1eze8aE'> </div>"
   end
