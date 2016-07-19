7ec53922346ee33f0b11b0a79791201619f2ae1c
liskowsky <karollebiedzinski@gmail.com>
2016-07-19 19:00:28 +0100

    14 using POST method

--- app.rb
+++ app.rb
@@ -13,8 +13,12 @@ require 'sinatra'
     erb(:index)
   end
 
-  get '/named-cat' do
+  post '/named-cat' do
     p params
     @name = params[:name]
     erb(:index)
+  end
+
+  get '/cat-form' do
+    erb(:cat_form)
   end
